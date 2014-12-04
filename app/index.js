bb07b81d9cbe79c7e7c17ce5ce4a45215999cc72
kazuhide maruyama <afurock@gmail.com>
2014-12-05 00:51:48 +0900

    project.jsonの生成方法を変更。将来的にはmoduleにする。

--- app/index.js
+++ app/index.js
@@ -7,89 +7,143 @@ var yosay = require("yosay");
 var uuid = require("uuid");
 
 var AspnetProjectGenerator = yeoman.generators.Base.extend({
-  initializing: function () {
-    this.pkg = require("../package.json");
-    this.opts = {
-        uuid: uuid.v4()
-    };
-  },
+    initializing: function() {
+        this.pkg = require("../package.json");
+        this.opts = {
+            uuid: uuid.v4()
+        };
+    },
 
-  prompting: function () {
-    var done = this.async(),
-        prompts = [],
-        isWin = !!require("os").platform().match(/^win/);
+    prompting: function() {
+        var done = this.async(),
+            prompts = [],
+            isWin = !!require("os").platform().match(/^win/);
 
-    // Have Yeoman greet the user.
-    this.log(yosay(
-      "Welcome to the good Asp.net 5 project generator!"
-    ));
+        // Have Yeoman greet the user.
+        this.log(yosay(
+            "Welcome to the good Asp.net 5 project generator!"
+        ));
 
-    prompts.push({
-        type: "input",
-        name: "projectName",
-        message: "Please enter a project name.",
-        "default": this._.capitalize(path.basename(this.env.cwd)) || "WebApplication",
-    }, {
-        type: "checkbox",
-        name: "server",
-        message: "Please select the web server.",
-        choices: [{
-            name: "IIS",
-            value: "iis",
-            checked: isWin
+        prompts.push({
+            type: "input",
+            name: "projectName",
+            message: "Please enter a project name.",
+            "default": this._.capitalize(path.basename(this.env.cwd)) || "WebApplication",
         }, {
-            name: ".Net web listener",
-            value: "weblistener",
-            checked: isWin
-        }, {
-            name: "kestrel",
-            value: "kestrel",
-            checked: !isWin
-        }],
-        validate: function(answer){
-            if(!answer.length){
-                return "Please checked one or more options.";
+            type: "checkbox",
+            name: "server",
+            message: "Please select the web server.",
+            choices: [{
+                name: "IIS",
+                value: "iis",
+                checked: isWin
+            }, {
+                name: ".Net web listener",
+                value: "weblistener",
+                checked: isWin
+            }, {
+                name: "kestrel",
+                value: "kestrel",
+                checked: !isWin
+            }],
+            validate: function(answer) {
+                if (!answer.length) {
+                    return "Please checked one or more options.";
+                }
+                return true;
             }
-            return true;
+        });
+
+        this.prompt(prompts, function(prop) {
+            this._.merge(this.opts, prop);
+            done();
+        }.bind(this));
+    },
+
+    writing: function() {
+        this.dest.mkdir("wwwroot");
+
+        this.expandFiles("**", {
+            cwd: this.sourceRoot()
+        }).forEach(function(file) {
+            if (this._.startsWith(file, "_")) {
+                this._tmpl(file, this._getReplacedFileName(file));
+            } else {
+                this.copy(file, file);
+            }
+        }.bind(this));
+
+        this.fs.write("project.json", this._buildProjectJson());
+    },
+
+    _tmpl: function(src, dest) {
+        this.fs.copyTpl(
+            this.templatePath(src),
+            this.destinationPath(dest || src),
+            this.opts
+        );
+    },
+
+    _getReplacedFileName: function(templateName) {
+        if (templateName === "_web.kproj") {
+            return this.opts.projectName + ".kproj";
         }
-    });
+        return templateName.replace(/^_/, "");
+    },
 
-    this.prompt(prompts, function(prop){
-        this._.merge(this.opts, prop);
-        done();
-    }.bind(this));
-  },
+    _buildProjectJson: function() {
+        var projectJson = {
+            version: "1.0.0-*",
+            webroot: "wwwroot",
+            exclude: ["wwwroot"],
+            dependencies: {
+                "Microsoft.AspNet.StaticFiles": "1.0.0-beta1",
+                "Microsoft.Framework.ConfigurationModel.Json": "1.0.0-beta1",
+                "Microsoft.AspNet.Hosting": "1.0.0-beta1"
+            },
+            commands: {},
+            frameworks: {
+                aspnet50: {},
+                aspnetcore50: {}
+            }
+        };
 
-  writing: function () {
-      this.dest.mkdir("wwwroot");
+        if(this.opts.server.indexOf("weblistener") > -1) {
+            projectJson = this._.merge(projectJson, {
+                dependencies: {
+                    "Microsoft.AspNet.Server.WebListener": "1.0.0-beta1"
+                },
+                commands: {
+                    "web": "Microsoft.AspNet.Hosting --server Microsoft.AspNet.Server.WebListener --server.urls http://localhost:5001"
+                }
+            });
+        }
 
-      this.expandFiles("**", { cwd: this.sourceRoot() }).forEach(function(file){
-         if(this._.startsWith(file, "_")){
-             this._tmpl(file, this._getReplacedFileName(file));
-         } else {
-             this.copy(file, file);
-         }
-     }.bind(this));
-  },
+        if(this.opts.server.indexOf("kestrel") > -1) {
+            projectJson = this._.merge(projectJson, {
+                dependencies: {
+                    "kestrel": "1.0.0-*"
+                },
+                commands: {
+                    "kestrel": "Microsoft.AspNet.Hosting --server Kestrel --server.urls http://localhost:5001"
+                }
+            });
+        }
 
-  _tmpl: function(src, dest){
-      this.fs.copyTpl(
-          this.templatePath(src),
-          this.destinationPath(dest || src),
-          this.opts
-      );
-  },
+        if(this.opts.server.indexOf("iis") > -1) {
+            projectJson = this._.merge(projectJson, {
+                dependencies: {
+                    "Microsoft.AspNet.Server.IIS": "1.0.0-beta1"
+                }
+            });
+        }
 
-  _getReplacedFileName: function(templateName){
-      if(templateName === "_web.kproj"){
-          return this.opts.projectName + ".kproj";
-      }
-      return templateName.replace(/^_/, "");
-  },
+        return JSON.stringify(projectJson, null, "    ");
+    },
 
-  end: function () {
+    end: function() {
 
-  }
+    }
 });
 
 module.exports = AspnetProjectGenerator;
